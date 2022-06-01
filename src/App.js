04aed2a218dbbb9cd17bb332a5f7f643c09308f9
Jakub Svihla <jsvihla@gmail.com>
2022-06-01 10:56:39 +0100

    adds a paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edits <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Added a paragraph.
         </p>
         <a
           className="App-link"
